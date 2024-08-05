654fbec638f3f932e85d8191a5720fb1e119b3db
iaroslav1970 <barayaros@gmail.com>
2024-08-05 16:20:11 +0300

    Creating model classes

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+namespace NorthwindIaroslav.Models {
+    public class Category
+{
+    public int CategoryID { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
+}
--- obj/Debug/net8.0/NorthwindIaroslav.AssemblyInfo.cs
+++ obj/Debug/net8.0/NorthwindIaroslav.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindIaroslav")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c20aab1d541eade38db07cad1c6dc58a7726b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindIaroslav")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindIaroslav")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
